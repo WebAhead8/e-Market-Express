7384a03ed17921eeabd3d96558a9ab8237c06823
Jihad Banna <super_banna@hotmail.com>
2021-02-23 10:31:03 +0200

    server

--- server.js
+++ server.js
@@ -1,1 +1,7 @@
+const express = require("express");
 
+const PORT = process.env.PORT || 4000;
+
+const server = express();
+
+server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
