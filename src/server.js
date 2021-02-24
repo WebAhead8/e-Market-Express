0229b52f6a58b99b9d82f901262e5cedf05db449
AlaaMath <alaa.lathqani@gmail.com>
2021-02-24 16:19:02 +0200

    alaa

--- src/server.js
+++ src/server.js
@@ -1,14 +1,17 @@
-const express = require("express");
-const productsHandler = require("./handlers/products.js");
+const express = require('express');
+const productsHandler = require('./handlers/products.js');
 
 const PORT = process.env.PORT || 4000;
 
 const server = express();
 
-server.get("/products", productsHandler.getAllProducts);
+// products
+server.get('/products', productsHandler.getAllProducts);
 
-server.get("/category/:cat", productsHandler.getCategory);
+server.get('/category/:cat', productsHandler.getCategory);
 
-server.get("/product/:name", productsHandler.getProduct);
+server.get('/product/:name', productsHandler.getProduct);
+
+// users
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
