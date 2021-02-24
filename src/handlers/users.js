0023e85efc742748b75e8f312a308ea287d0355b
Jihad Banna <super_banna@hotmail.com>
2021-02-24 14:51:25 +0200

    signup

--- src/handlers/users.js
+++ src/handlers/users.js
@@ -7,3 +7,17 @@ const usersModel = require("../model/users.js");
 // Mahmoud
 
 // Jihad
+
+function signUp(req, res, next) {
+  const newUser = req.body;
+  console.log(req.body);
+  usersModel
+    .signUp(newUser)
+    .then((user) => {
+      console.log(user);
+      const response = user.rows[0].row;
+
+      res.status(201).send(response);
+    })
+    .catch(next);
+}
--- src/model/users.js
+++ src/model/users.js
@@ -7,3 +7,16 @@ const db = require("../database/connection.js");
 // Mahmoud
 
 // Jihad
+
+function signUp(newUser) {
+  return db.query(
+    "INSERT INTO users (email, first_name, last_name, password, role) VALUES ($1,$2,$3,$4,$5)  RETURNING(email, first_name, role)",
+    [
+      newUser.email,
+      newUser.first_name,
+      newUser.last_name,
+      newUser.password,
+      "client",
+    ]
+  );
+}
