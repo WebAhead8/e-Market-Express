f7655b3a834a2f8ea153e7c964a9870a658b822f
AlaaMath <alaa.lathqani@gmail.com>
2021-02-25 11:45:23 +0200

    logout

--- src/server.js
+++ src/server.js
@@ -1,39 +1,24 @@
-<<<<<<< HEAD
 const express = require('express');
-const productsHandler = require('./handlers/products.js');
-=======
-const express = require("express");
-const productsHandler = require("./handlers/products");
-const usersHandler = require("./handlers/users");
-const handleError = require("./middleware/error");
->>>>>>> 20cac40ab2280e4aa04bf2d5254eb82adfc5217f
+const productsHandler = require('./handlers/products');
+const usersHandler = require('./handlers/users');
+const handleError = require('./middleware/error');
 
 const PORT = process.env.PORT || 4000;
 
 const server = express();
 
-<<<<<<< HEAD
-// products
-server.get('/products', productsHandler.getAllProducts);
-
-server.get('/category/:cat', productsHandler.getCategory);
-
-server.get('/product/:name', productsHandler.getProduct);
-
-// users
-=======
 server.use(express.json());
 
-server.get("/products", productsHandler.getAllProducts);
-server.get("/category/:cat", productsHandler.getCategory);
-server.get("/product/:name", productsHandler.getProduct);
-server.delete("/del/:id", productsHandler.del);
+server.get('/products', productsHandler.getAllProducts);
+server.get('/category/:cat', productsHandler.getCategory);
+server.get('/product/:name', productsHandler.getProduct);
+server.delete('/del/:id', productsHandler.del);
 
 // users
-server.post("/login", usersHandler.login);
-server.post("/signup", usersHandler.signUp);
+server.post('/login', usersHandler.login);
+server.post('/signup', usersHandler.signUp);
+server.get('/logout', usersHandler.logOut);
 
 server.use(handleError);
->>>>>>> 20cac40ab2280e4aa04bf2d5254eb82adfc5217f
 
 server.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}`));
