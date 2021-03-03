e0c26ce895b14de527ef5899b690d6e8d54c2fd1
mahmoud Diab <mhmoddiab2@gmail.com>
2021-03-03 15:11:35 +0200

    add horuko

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,7 +1,14 @@
 const pg = require("pg");
 const dotenv = require("dotenv");
 dotenv.config();
-const connectionString = process.env.DATABASE_URL;
+let connectionString;
+
+if (process.env.NODE_ENV === "production") {
+  connectionString = process.env.DATABASE_URL;
+} else {
+  connectionString = process.env.DATABASE_URL_DEV;
+}
+
 const db = new pg.Pool({ connectionString });
 
 module.exports = db;
